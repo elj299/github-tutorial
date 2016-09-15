088864bafbd8f318e1864f59d12176d9ecf6f8c3
eleishaj@utexas.edu <eleishajackson@Eleishas-MacBook-Pro-2.local>
2016-09-15 13:05:58 -0700

    Adding art

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -1,11 +1,12 @@
-
+def print_art():
+    print "#### ##### #### ####"
 
 def print_message():
     print "Printing a message!!!!"
 
 def main():
     print_message()
-
+    print_art()
 
 if __name__ == "__main__":
     main()
