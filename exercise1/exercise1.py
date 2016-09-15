e6dac990ef8b134b02e140c092c58c5acea7941c
eleishaj@utexas.edu <eleishajackson@Eleishas-MacBook-Pro-2.local>
2016-09-15 13:08:57 -0700

    Adding last function

--- exercise1/exercise1.py
+++ exercise1/exercise1.py
@@ -2,8 +2,13 @@ def print_art():
     print "#### ##### #### ####"
 
 def print_message():
+    
     print "Printing a message!!!!"
 
+
+def print_hi_fellow(str = "I am an Insight Fellow!"):
+    print "That's awesome! How is that random forest model going?"
+ 
 def main():
     print_message()
     print_art()
